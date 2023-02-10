0f7b32291ad15a4ee4c6eb09d8177ad4261184d9
Ziggote <cleanchad1983@gmail.com>
2023-02-10 10:18:58 -0600

    Add files via upload

---
+++ SonarrParsarr.py
@@ -0,0 +1,221 @@
+# import requests
+# import csv
+
+# # API key & endpoint
+# api_key = "2d763e4c7e294686a5d7e553518b1260"
+# headers = {"X-Api-Key": api_key}
+
+# response = requests.get("http://localhost:8989/api/queue", headers=headers)
+# if response.status_code == 200:
+#     data = response.json()
+#     with open("sonarTestarrInfo.csv", "w", newline="") as file:
+#         writer = csv.writer(file)
+#         writer.writerow(["Title", "Download ID", "ID", "Sizeleft", "Size", "Status"])
+#         for torrent in data:
+#             title = torrent.get("title", "")
+#             download_id = torrent.get("downloadId", "")
+#             id = torrent.get("id", "")
+#             sizeleft = torrent.get("sizeleft", "")
+#             size = torrent.get("size", "")
+#             status = torrent.get("status", "")
+
+#             print("Title:", title)
+#             print("Download ID:", download_id)
+#             print("ID:", id)
+#             print("Sizeleft:", sizeleft)
+#             print("Size:", size)
+#             print("\n")
+#             # print(data)
+#             writer.writerow([title, download_id, id, sizeleft, size, status])
+# else:
+#     print("Request failed with status code", response.status_code)
+
+######################################################################################
+#                        ^   ^   ^   ^   ^   ^   ^   ^
+# ****************   ORIGINAL WORKING CODE, DO NOT ALTER   *********************
+#                        ^   ^   ^   ^   ^   ^   ^   ^
+######################################################################################
+
+# import requests
+# import csv
+
+# # API key & endpoint
+# api_key = "2d763e4c7e294686a5d7e553518b1260"
+# headers = {"X-Api-Key": api_key}
+
+# try:
+#     response = requests.get("http://localhost:8989/api/queue", headers=headers)
+#     if response.status_code == 200:
+#         try:
+#             data = response.json()
+#         except ValueError as e:
+#             print(f"Error parsing response: {e}")
+#             data = []
+
+#         # Keep track of strikes for each torrent
+#         strikes = {}
+
+#         try:
+#             with open("sonarTestarrInfo.csv", "r") as file:
+#                 reader = csv.DictReader(file)
+#                 for row in reader:
+#                     # Read the existing strikes from the file
+#                     download_id = row["Download ID"]
+#                     strikes[download_id] = int(row["Strike"])
+#         except FileNotFoundError as e:
+#             print(f"File not found: {e}")
+#         except PermissionError as e:
+#             print(f"Permission error: {e}")
+
+#         try:
+#             with open("sonarTestarrInfo.csv", "w", newline="") as file:
+#                 writer = csv.writer(file)
+#                 writer.writerow(
+#                     [
+#                         "Title",
+#                         "Download ID",
+#                         "ID",
+#                         "Sizeleft",
+#                         "Size",
+#                         "Status",
+#                         "Strike",
+#                     ]
+#                 )
+#                 for torrent in data:
+#                     title = torrent.get("title", "")
+#                     download_id = torrent.get("downloadId", "")
+#                     id = torrent.get("id", "")
+#                     sizeleft = torrent.get("sizeleft", "")
+#                     size = torrent.get("size", "")
+#                     status = torrent.get("status", "")
+
+#                     # Increment the strike count if the status is 'Warning'
+#                     if status == "Warning":
+#                         strikes[download_id] = strikes.get(download_id, 0) + 1
+#                     elif status == "Downloading" and strikes.get(download_id, 0) > 0:
+#                         strikes[download_id] = strikes.get(download_id, 0) - 1
+
+#                     print("Title:", title)
+#                     print("Download ID:", download_id)
+#                     print("ID:", id)
+#                     print("Sizeleft:", sizeleft)
+#                     print("Size:", size)
+#                     print("Status:", status)
+#                     print("Strike:", strikes.get(download_id, 0))
+#                     print("\n")
+#                     writer.writerow(
+#                         [
+#                             title,
+#                             download_id,
+#                             id,
+#                             sizeleft,
+#                             size,
+#                             status,
+#                             strikes.get(download_id, 0),
+#                         ]
+#                     )
+#         except Exception as e:
+#             print(f"Error writing to file: {e}")
+#     else:
+#         print("Request failed with status code", response.status_code)
+# except Exception as e:
+#     print(f"Error making request: {e}")
+
+######################################################################################
+#                        ^   ^   ^   ^   ^   ^   ^   ^
+# ****************            WORKING STRIKE SYSTEM          *********************
+#                        ^   ^   ^   ^   ^   ^   ^   ^
+######################################################################################
+
+
+import requests
+import csv
+
+# API key & endpoint
+api_key = "2d763e4c7e294686a5d7e553518b1260"
+headers = {"X-Api-Key": api_key}
+
+response = requests.get("http://localhost:8989/api/queue", headers=headers)
+if response.status_code == 200:
+    data = response.json()
+
+    # Keep track of strikes for each torrent
+    strikes = {}
+
+    with open("sonarTestarrInfo.csv", "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            # Read the existing strikes for each torrent from the file
+            download_id = row["Download ID"]
+            strikes[download_id] = int(row["Strike"])
+
+    with open("sonarTestarrInfo.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            ["Title", "Download ID", "ID", "Sizeleft", "Size", "Status", "Strike"]
+        )
+        for torrent in data:
+            title = torrent.get("title", "")
+            download_id = torrent.get("downloadId[-5:]", "")
+            id = torrent.get("id", "")
+            sizeleft = torrent.get("sizeleft", "")
+            size = torrent.get("size", "")
+            status = torrent.get("status", "")
+
+            # Initialize the strikes for each torrent in an individual dictionary
+            torrent_strikes = {download_id: strikes.get(download_id, 0)}
+
+            # Increment the strike count for the current torrent if its status is 'Warning'
+            if status == "Warning":
+                torrent_strikes[download_id] += 1
+                strikes[download_id] = torrent_strikes[download_id]
+
+            # Remove a strike if the status is 'Downloading'
+            if status == "Downloading":
+                if torrent_strikes[download_id] > 0:
+                    torrent_strikes[download_id] -= 1
+                    strikes[download_id] = torrent_strikes[download_id]
+
+            # Remove the torrent if it has 3 or more strikes
+            if torrent_strikes[download_id] >= 3:
+                delete_response = requests.delete(
+                    "http://localhost:8989/api/v3/queue/{}?removeFromClient=true&blocklist=true&apikey={}".format(
+                        id, api_key
+                    )
+                )
+                if delete_response.status_code == 200:
+                    print("Torrent: {} removed".format(title))
+                    # Remove the torrent from the strikes dictionary
+                    strikes.pop(download_id, None)
+                    # Update the torrent_strikes dictionary to 0 strikes after removal
+                    torrent_strikes[download_id] = 0
+                else:
+                    print(
+                        "Failed to remove torrent with download ID {} and status code {}".format(
+                            download_id, delete_response.status_code
+                        )
+                    )
+
+            # print("Title:", title)
+            # print("Download ID:", download_id)
+            # print("ID:", id)
+            # print("Sizeleft:", sizeleft)
+            # print("Size:", size)
+            # print("Status:", status)
+            # print("Strike:", torrent_strikes[download_id])
+            # print("\n")
+            writer.writerow(
+                [
+                    title,
+                    download_id,
+                    id,
+                    sizeleft,
+                    size,
+                    status,
+                    torrent_strikes[download_id],
+                ]
+            )
+
+
+else:
+    print("Request failed with status code", response.status_code)
